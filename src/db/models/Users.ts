803e0ae6ad7a00ee4785c9dece7b05bb8c86a9a3
root <root@DESKTOP-TUHIIIT.localdomain>
2022-01-20 22:54:09 -0400

    deploy

--- src/db/models/Users.ts
+++ src/db/models/Users.ts
@@ -7,21 +7,30 @@ import {
 	PrimaryGeneratedColumn,
 	UpdateDateColumn,
 } from 'typeorm';
-import { Length, IsEmail, IsDate } from 'class-validator';
+import { Length, IsEmail, IsDate, isLowercase } from 'class-validator';
 // import R_User_Rols from './R_User_Rols';
 import R_User_Options from './R_User_Options';
 import { JoinTable, ManyToMany } from 'typeorm';
 import Rols from './Rols';
 
+const transformer = () => {
+
+	const to = (value: string) => value.toLowerCase();
+	const from = (value: string) => value.toLowerCase();
+
+	return { to, from }
+}
+
 @Entity(/*{ name: 'Users' }*/)
 export default class Users {
+
 	@PrimaryGeneratedColumn()
 	public id?: number;
 
 	@Column()
 	public name!: string;
 
-	@Column({ unique: true })
+	@Column({ transformer: transformer(), unique: true })
 	@IsEmail()
 	public email!: string;
 
